8e10859942c8289bf780692a4e87aa0b36530916
Matthias Viehweger <viehweger@puzzle.ch>
2018-02-05 18:44:08 +0100

    Outline some specs

--- spec/gpuzzletime_spec.rb
+++ spec/gpuzzletime_spec.rb
@@ -7,7 +7,17 @@ describe Gpuzzletime do
     expect(Gpuzzletime::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  it 'has a configurable puzzletime-domain'
+
+  it 'can parse log-entries'
+  it 'omits entries ending in two stars'
+
+  it 'knows some dates by relative names'
+
+  it 'can show parsed entries'
+  it 'can upload parsed entries'
+  it 'omits empty dates'
+  it 'can limit entries to one day'
+
+  it 'can load custom mappers for the ordertime_account'
 end

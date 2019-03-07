4a80b87f67f97f8e23176863d9f7ebf9a5c79f56
Matthias Viehweger <viehweger@puzzle.ch>
2019-03-07 10:40:25 +0100

    Conform to linter and styleguide

--- spec/gpuzzletime/app_spec.rb
+++ spec/gpuzzletime/app_spec.rb
@@ -8,22 +8,22 @@ describe Gpuzzletime::App do
   let(:command) { 'show' }
   let(:argument) { nil }
   let(:timelog) do
-    <<-TIMELOG
-2018-03-02 09:51: start
-2018-03-02 11:40: 12345: prepare deployment -- webapp
-2018-03-02 12:25: lunch **
-2018-03-02 13:15: 12345: prepare deployment -- webapp
-2018-03-02 14:30: break **
-2018-03-02 16:00: handover
-2018-03-02 17:18: cleanup database
-2018-03-02 18:58: break **
-2018-03-02 20:08: 12345: prepare deployment -- webapp
+    <<~TIMELOG
+      2018-03-02 09:51: start
+      2018-03-02 11:40: 12345: prepare deployment -- webapp
+      2018-03-02 12:25: lunch **
+      2018-03-02 13:15: 12345: prepare deployment -- webapp
+      2018-03-02 14:30: break **
+      2018-03-02 16:00: handover
+      2018-03-02 17:18: cleanup database
+      2018-03-02 18:58: break **
+      2018-03-02 20:08: 12345: prepare deployment -- webapp
 
-2018-03-03 14:00: start
-2018-03-03 15:34: 23456: debug -- network
-2018-03-03 18:46: studying
-2018-03-03 20:08: dinner **
-2018-03-03 21:36: 12345: prepare deployment -- webapp
+      2018-03-03 14:00: start
+      2018-03-03 15:34: 23456: debug -- network
+      2018-03-03 18:46: studying
+      2018-03-03 20:08: dinner **
+      2018-03-03 21:36: 12345: prepare deployment -- webapp
     TIMELOG
   end
 

e2e90a66d3817dd5b70857e509ab8a70f44c5b37
Matthias Viehweger <viehweger@puzzle.ch>
2020-01-22 14:21:18 +0100

    Extend documentation

--- lib/ptimelog/version.rb
+++ lib/ptimelog/version.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
-# Version
+# the version, following semver
+#
+# Someone wanted to have documentation, so here goes...
+#
+# Please note that the version-string is not frozen. Although it of course is,
+# because all strings in this file are frozen. That's what the magic comment
+# at the top of the file does. :gasp:
+#
+# What else? Yeah, the VERSION-constant is part of the module Ptimelog because
+# that is what is described here.
+#
+# So, I truly hope you are happy now, that I documented this file properly. For
+# any remaining questions, please open an issue or even better a pull-request
+# with an improvement. Keep in mind that this is also covered by rspec so I
+# expect (pun intended) 100% test-coverage for any additional code.
 module Ptimelog
   VERSION = '0.5.3'
 end

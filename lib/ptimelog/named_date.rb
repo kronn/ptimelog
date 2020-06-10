f2869b2b1b6ecc18145c69c73fe2a85a2c30d081
Matthias Viehweger <viehweger@puzzle.ch>
2020-06-10 08:09:50 +0200

    Support weekdays of last week

--- lib/ptimelog/named_date.rb
+++ lib/ptimelog/named_date.rb
@@ -9,16 +9,25 @@ module Ptimelog
       named_date(arg) || :all
     end
 
-    def named_date(date)
+    def named_date(date) # rubocop:disable Metrics/CyclomaticComplexity
       case date.to_s
       when 'yesterday'        then yesterday.to_s
       when 'today'            then Date.today.to_s
       when 'last', ''         then last_entry.to_s || yesterday.to_s
+      when /(mon|tues|wednes|thurs|fri|satur|sun)day/
+        previous_weekday(date).to_s
       when /\d{4}(-\d{2}){2}/ then date
       end
     end
 
     private
+
+    def previous_weekday(date)
+      Date.today.prev_day(7)
+          .step(Date.today.prev_day)
+          .find { |d| d.send(:"#{date}?") }
+          .to_s
+    end
 
     def last_entry
       timelog.to_h.keys.compact.sort[-2]
--- spec/ptimelog/named_date_spec.rb
+++ spec/ptimelog/named_date_spec.rb
@@ -22,6 +22,92 @@ describe Ptimelog::NamedDate do
     end
   end
 
+  context 'weekdays are known by name:' do
+    it 'monday' do
+      today = '2018-03-05'
+      monday = '2018-02-26'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(monday)).to be_monday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('monday')).to eq(monday)
+      end
+    end
+
+    it 'tuesday' do
+      today = '2018-03-05'
+      tuesday = '2018-02-27'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(tuesday)).to be_tuesday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('tuesday')).to eq(tuesday)
+      end
+    end
+
+    it 'wednesday' do
+      today = '2018-03-05'
+      wednesday = '2018-02-28'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(wednesday)).to be_wednesday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('wednesday')).to eq(wednesday)
+      end
+    end
+
+    it 'thursday' do
+      today = '2018-03-05'
+      thursday = '2018-03-01'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(thursday)).to be_thursday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('thursday')).to eq(thursday)
+      end
+    end
+
+    it 'friday' do
+      today = '2018-03-05'
+      friday = '2018-03-02'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(friday)).to be_friday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('friday')).to eq(friday)
+      end
+    end
+
+    it 'saturday' do
+      today = '2018-03-05'
+      saturday = '2018-03-03'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(saturday)).to be_saturday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('saturday')).to eq(saturday)
+      end
+    end
+
+    it 'sunday' do
+      today = '2018-03-05'
+      sunday = '2018-03-04'
+
+      expect(Date.parse(today)).to be_monday
+      expect(Date.parse(sunday)).to be_sunday
+
+      Timecop.travel(today) do
+        expect(subject.named_date('sunday')).to eq(sunday)
+      end
+    end
+  end
+
   it 'understands and accepts dates in YYYY-MM-DD format' do
     date = '1970-01-01'
     expect(subject.named_date(date)).to be date

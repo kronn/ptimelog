86bf70097d8b75a7b76170501c549ffa547a9f11
Matthias Viehweger <viehweger@puzzle.ch>
2020-06-10 08:08:16 +0200

    Extract non-trivial nameable days into methods

--- lib/ptimelog/named_date.rb
+++ lib/ptimelog/named_date.rb
@@ -11,14 +11,22 @@ module Ptimelog
 
     def named_date(date)
       case date.to_s
-      when 'yesterday'        then Date.today.prev_day.to_s
+      when 'yesterday'        then yesterday.to_s
       when 'today'            then Date.today.to_s
-      when 'last', ''         then timelog.to_h.keys.compact.sort[-2] || Date.today.prev_day.to_s
+      when 'last', ''         then last_entry.to_s || yesterday.to_s
       when /\d{4}(-\d{2}){2}/ then date
       end
     end
 
     private
+
+    def last_entry
+      timelog.to_h.keys.compact.sort[-2]
+    end
+
+    def yesterday
+      Date.today.prev_day
+    end
 
     def timelog
       Timelog.load
